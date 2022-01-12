9469664710215fdc61f83658ed0b1b4d3f2c0221
justbegginer <46086383+justbegginer@users.noreply.github.com>
2022-01-12 22:55:59 +0300

    final code with all types of enemy

--- src/test/java/factory/EnemyTest.java
+++ src/test/java/factory/EnemyTest.java
@@ -1,23 +1,37 @@
 package factory;
 
+import factory.enemy.rarity_gradation.Enemy;
+import factory.enemy.rarity_gradation.Rarity;
+import factory.factory.AverageEnemyFactory;
+import factory.factory.BigEnemyFactory;
+import factory.factory.EnemyFactory;
+
+import factory.factory.FastEnemyFactory;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
 class EnemyTest {
+    static class Task{
+        public static void task(){
+            forAllSpecification(Rarity.SIMPLE);
+            forAllSpecification(Rarity.COMMON);
+            forAllSpecification(Rarity.RARE);
+            forAllSpecification(Rarity.EPIC);
+            forAllSpecification(Rarity.LEGENDARY);
+
+        }
+        private static void forAllSpecification(Rarity rarity){
+            EnemyFactory factory = new AverageEnemyFactory();
+            System.out.println(factory.create(rarity));
+            factory = new BigEnemyFactory();
+            System.out.println(factory.create(rarity));
+            factory = new FastEnemyFactory();
+            System.out.println(factory.create(rarity));
+        }
+    }
     @Test
     void factory(){
-        EnemyFactory enemyFactory = new FastEnemyFactory();
-        Enemy enemy = enemyFactory.create();
-        System.out.println("Must be FAST enemy");
-        System.out.println(enemy);
-        enemyFactory = new AverageEnemyFactory();
-        enemy = enemyFactory.create();
-        System.out.println("Must be AVERAGE enemy");
-        System.out.println(enemy);
-        enemyFactory = new BigEnemyFactory();
-        enemy = enemyFactory.create();
-        System.out.println("Must be BIG enemy");
-        System.out.println(enemy);
+        Task.task();
     }
 }
